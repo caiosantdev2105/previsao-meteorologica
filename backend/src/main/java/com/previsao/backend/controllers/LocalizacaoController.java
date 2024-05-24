e18a0d51dc9915419084b4d297dce0ee006bb67c
Caio Santana <caiosantanaprofissional@gmail.com>
2024-05-24 07:51:43 -0300

    Revert "Implementação do EndPoint Controlador da Localização"
    This reverts commit 9ec835a7ef366e826e540c3df9aeec9f69486815.

--- backend/src/main/java/com/previsao/backend/controllers/LocalizacaoController.java
+++ backend/src/main/java/com/previsao/backend/controllers/LocalizacaoController.java
@@ -6,24 +6,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.previsao.backend.models.Clima;
-import com.previsao.backend.models.Localizacao;
 import com.previsao.backend.services.ClimaServices;
 import com.previsao.backend.services.LocalizacaoServices;
 
-//Controlador da Localização
 @RestController
 public class LocalizacaoController {
 
-    //Injeção dos serviços (lógica) da localização
     @Autowired
     private LocalizacaoServices servicosLocalizacao;
-
-    //
-    @PostMapping("/previsao/localizacao")
-    public String obterLocalizacao(@RequestBody Localizacao cidade) {
-        String response = servicosLocalizacao.obterCoordenadas(cidade);
-
-        return response;
-    }
 
 }
--- backend/src/main/java/com/previsao/backend/services/LocalizacaoServices.java
+++ backend/src/main/java/com/previsao/backend/services/LocalizacaoServices.java
@@ -1,8 +1,5 @@
 package com.previsao.backend.services;
 
 public class LocalizacaoServices {
-    public String obterCoordenadas (nomeCidade) {
-        
-        return resp;
-    }
+    
 }
