0136f9f6c0fcaa25e105be2e5553b2e44e656631
Caio Santana <caiosantanaprofissional@gmail.com>
2024-05-24 10:27:14 -0300

    Criação da rota para envio do nome da cidade

--- backend/src/main/java/com/previsao/backend/controllers/LocalizacaoController.java
+++ backend/src/main/java/com/previsao/backend/controllers/LocalizacaoController.java
@@ -1,12 +1,13 @@
 package com.previsao.backend.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.previsao.backend.models.Clima;
-import com.previsao.backend.services.ClimaServices;
+import com.previsao.backend.dto.LocalizacaoDTO;
 import com.previsao.backend.services.LocalizacaoServices;
 
 @RestController
@@ -14,5 +15,13 @@ public class LocalizacaoController {
 
     @Autowired
     private LocalizacaoServices servicosLocalizacao;
+
+    @PostMapping("/backend/coordenadas")
+    public List<LocalizacaoDTO> getCoordenadas(@RequestBody String nomeCidade) throws Exception {
+
+        List<LocalizacaoDTO> resposta = servicosLocalizacao.getLocalizacao(nomeCidade);
+
+        return resposta;
+    }
 
 }
--- backend/src/main/java/com/previsao/backend/services/ClimaServices.java
+++ backend/src/main/java/com/previsao/backend/services/ClimaServices.java
@@ -1,5 +1,8 @@
 package com.previsao.backend.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClimaServices {
 
 }
