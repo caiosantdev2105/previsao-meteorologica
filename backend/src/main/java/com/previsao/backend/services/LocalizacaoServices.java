9caec1af9e09360b7057200fc382246a573f2dc4
Caio Santana <caiosantanaprofissional@gmail.com>
2024-05-24 10:26:45 -0300

    Criação da Lógica para requisição da API terceira de coordenadas

--- backend/src/main/java/com/previsao/backend/services/LocalizacaoServices.java
+++ backend/src/main/java/com/previsao/backend/services/LocalizacaoServices.java
@@ -2,24 +2,37 @@ package com.previsao.backend.services;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.previsao.backend.dto.LocalizacaoDTO;
 
+@Service
 public class LocalizacaoServices {
-    public LocalizacaoDTO getLocalizacao(String nomeCidade) throws IOException, InterruptedException {
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://api.openweathermap.org/geo/1.0/direct?q=" + nomeCidade
-                            + ",BR&limit=1&appid=aa64c985297e12b2d3e6d1a566f41b78"))
-                    .build();
 
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (Exception error) {
-            System.out.println(error);
-        }
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    public LocalizacaoServices() {
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public List<LocalizacaoDTO> getLocalizacao(String nomeCidade) throws Exception {
+        String encodedCityName = URLEncoder.encode(nomeCidade, "UTF-8");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://api.openweathermap.org/geo/1.0/direct?q=" + encodedCityName
+                        + ",BR&limit=1&appid=aa64c985297e12b2d3e6d1a566f41b78"))
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        return objectMapper.readValue(response.body(), new TypeReference<List<LocalizacaoDTO>>() {
+        });
     }
 }
