a901200de424f0fbe700bda2606e5a5038f8e6e9
Caio Santana <caiosantanaprofissional@gmail.com>
2024-05-24 07:30:46 -0300

    Estruturação dos Controllers

--- backend/src/main/java/com/previsao/backend/controllers/ClimaController.java
+++ backend/src/main/java/com/previsao/backend/controllers/ClimaController.java
@@ -1,5 +1,14 @@
 package com.previsao.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.previsao.backend.services.ClimaServices;
+
+@RestController
 public class ClimaController {
-    
+
+    @Autowired
+    private ClimaServices servicosClima;
+
 }
--- backend/src/main/java/com/previsao/backend/controllers/LocalizacaoController.java
+++ backend/src/main/java/com/previsao/backend/controllers/LocalizacaoController.java
@@ -1,5 +1,18 @@
 package com.previsao.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.previsao.backend.models.Clima;
+import com.previsao.backend.services.ClimaServices;
+import com.previsao.backend.services.LocalizacaoServices;
+
+@RestController
 public class LocalizacaoController {
-    
+
+    @Autowired
+    private LocalizacaoServices servicosLocalizacao;
+
 }
