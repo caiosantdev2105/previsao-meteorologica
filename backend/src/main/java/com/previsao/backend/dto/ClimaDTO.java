72f2534c68d698401d2f851716b3af4c2e44eae7
Caio Santana <caiosantanaprofissional@gmail.com>
2024-05-24 10:50:38 -0300

    Criação dos campos da DTO do Clima para o Jackson

--- backend/src/main/java/com/previsao/backend/dto/ClimaDTO.java
+++ backend/src/main/java/com/previsao/backend/dto/ClimaDTO.java
@@ -1,4 +1,78 @@
 package com.previsao.backend.dto;
 
-public class ClimaDTO { 
+public class ClimaDTO {
+    private String city_name;
+    private String date;
+    private double temp;
+    private double max;
+    private double min;
+    private String condition;
+
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public double getRain_probability() {
+        return rain_probability;
+    }
+
+    public void setRain_probability(double rain_probability) {
+        this.rain_probability = rain_probability;
+    }
+
+    public String getMoon_phase() {
+        return moon_phase;
+    }
+
+    public void setMoon_phase(String moon_phase) {
+        this.moon_phase = moon_phase;
+    }
+
+    private double rain_probability;
+    private String moon_phase;
+
 }
